f1719103974b6988f68a6fbc05d1bc89a4311ea0
vcian <vcian@uw.edu>
2021-09-09 13:18:09 -0700

    isolated pubmed calls

--- code/0_sysreview.r
+++ code/0_sysreview.r
@@ -4,7 +4,10 @@
 # ##############################################################################
 
 rm(list = ls())
+.libPaths("/ihme/homes/vcian/code/rlibs/")
 pacman::p_load(data.table, tidyverse, rio, medrxivr, easyPubMed, RISmed)
+
+setwd("H:/covid/covidcompare/")
 
 ## Toggle to update cache
 update <- 1
@@ -16,15 +19,23 @@ if (update) {
   ## Update Medrxiv pull
   df.mr <- import("data/review/medrxiv_cache.rds") %>% data.table()
   date.lastpull <- df.mr$date %>% max()
-  df.mr.new <- mx_api_content(from.date = date.lastpull) %>% data.table()
-  df.mr <- rbind(df.mr, df.mr.new)
+  df.mr.new <- mx_api_content(from_date = date.lastpull) %>% data.table()
+  df.mr <- rbind(df.mr, df.mr.new %>% rename(link = link_page, pdf = link_pdf) %>% select(-jatsxml))
   df.mr <- unique(df.mr, by = "doi")
   ## Save
   saveRDS(df.mr, "data/review/medrxiv_cache.rds")
   
   ## Update Pubmed pull
   date.new <- Sys.Date() %>% format("%Y/%m/%d")
-  search <- paste0("(covid[tiab] OR coronavirus[tiab]) AND (project*[tiab] OR forecast*[tiab]) AND (2019/01/01:", date.new, "[edat])")
+}
+ 
+if (F) { 
+  # with "model": n = 500
+  search <- paste0("(covid[tiab] OR coronavirus[tiab]) AND (project*[tiab] OR forecast*[tiab] OR scenario*[tiab] OR model*[tiab]) AND (2019/01/01:", date.new, "[edat])")
+  
+  # without model: n = 496
+  search <- paste0("(covid[tiab] OR coronavirus[tiab]) AND (project*[tiab] OR forecast*[tiab] OR scenario*[tiab]) AND (2019/01/01:", date.new, "[edat])")
+  
   df.xml <- search %>%
     get_pubmed_ids() %>%
     fetch_pubmed_data() %>%
@@ -41,17 +52,26 @@ mr <- import("data/review/medrxiv_cache.rds") %>%
   data.table() %>%
   select("doi", "title", "abstract", "date") %>%
   mutate(source = "medrxiv", date = as.Date(date))
-mr <- mr[(grepl("covid|coronavirus", title, ignore.case = TRUE) | grepl("covid|coronavirus", abstract, ignore.case = TRUE)) &
-           (grepl("projection|forecast", title, ignore.case = TRUE) | grepl("projection|forecast", abstract, ignore.case = TRUE))]
-pm <- import("data/review/pubmed_cache.rds") %>%
-  data.table() %>%
-  select("pmid", "doi", "title", "abstract", "day", "month", "year") %>%
-  mutate(source = "pubmed", pmid = as.numeric(pmid), date = as.Date(paste0(year, "-", month, "-", day))) %>%
-  select(!c("month", "day", "year"))
 
-## Combine lists
-df.new <- rbind(mr, pm, fill = T)
-df.new <- unique(df, by = "doi")
+# with "model": n = 4959
+# mr <- mr[(grepl("covid|coronavirus", title, ignore.case = TRUE) | grepl("covid|coronavirus", abstract, ignore.case = TRUE)) &
+# (grepl("projection|forecast|scenario|model", title, ignore.case = TRUE) | grepl("projection|forecast|scenario|model", abstract, ignore.case = TRUE))]
+
+# without "model": n = 1342
+mr <- mr[(grepl("covid|coronavirus", title, ignore.case = TRUE) | grepl("covid|coronavirus", abstract, ignore.case = TRUE)) &
+           (grepl("projection|forecast|scenario", title, ignore.case = TRUE) | grepl("projection|forecast|scenario", abstract, ignore.case = TRUE))]
+if (F) {
+  pm <- import("data/review/pubmed_cache.rds") %>%
+    data.table() %>%
+    select("pmid", "doi", "title", "abstract", "day", "month", "year") %>%
+    mutate(source = "pubmed", pmid = as.numeric(pmid), date = as.Date(paste0(year, "-", month, "-", day))) %>%
+    select(!c("month", "day", "year"))
+  
+  ## Combine lists
+  df.new <- rbind(mr, pm, fill = T)
+}
+
+df.new <- unique(df.new, by = "doi")
 df.new[, date := date %>% as.character()]
 df.new[, id := .I]
 
